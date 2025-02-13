197808eaaac072e4d0ea24af31de1a5251f861c5
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-13 22:39:30 +0530

    Update Footer.jsx

--- src/components/fcomponents/Footer.jsx
+++ src/components/fcomponents/Footer.jsx
@@ -1,71 +1,93 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
+      <footer id="footer">
+        <div class="footer-top">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-md-6 footer-contact">
+                <h3>पाल समाज विवाह</h3>
+                <p>
+                  बगहाई <br />
+                  रामपुर बाघेलान, जिला - सतना
+                  <br />
+                  भारत, मध्य प्रदेश
+                  <br />
+                  <br />
+                  <strong>Phone:</strong> 7770838846
+                  <br />
+                  <strong>Email:</strong> maneeshpal85@gmail.com
+                  <br />
+                </p>
+              </div>
 
-<footer id="footer">
+              <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Quick Links</h4>
+                <ul>
+                  <li>
+                    {" "}
+                    <a href="/">Home</a>
+                  </li>
+                </ul>
+              </div>
 
+              <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Join with us</h4>
+                <ul>
+                  <li>
+                    {" "}
+                    <a href="/signup">Register</a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/login">Login</a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/admin">Admin Login</a>
+                  </li>
+                </ul>
+              </div>
 
-
-<div class="footer-top">
-  <div class="container">
-    <div class="row">
-
-      <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>Perfect Match</h3>
-        <p>
-          Fron of court <br/>
-          Khanapur, Belgaum<br/>
-          India, Karnataka<br/><br/>
-          <strong>Phone:</strong> 8867137467<br/>
-          <strong>Email:</strong> info@perfectmatch.com<br/>
-        </p>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Quick Links</h4>
-        <ul>
-          <li> <a href="/">Home</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Join with us</h4>
-        <ul>
-          <li> <a href="/signup">Register</a></li>
-          <li> <a href="/login">Login</a></li>
-          <li> <a href="/admin">Admin Login</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Social Networks</h4>
-        <p>Connect with us on below platforms. All updates will be posted here...</p>
-        <div class="social-links mt-3">
-          <a href="/" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="/" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="/" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="/" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Our Social Networks</h4>
+                <p>
+                  हमसे नीचे दिए गए प्लेटफ़ॉर्म पर जुड़ें। सभी अपडेट यहीं पोस्ट
+                  किए जाएंगे...
+                </p>
+                <div class="social-links mt-3">
+                  <a href="/" class="twitter">
+                    <i class="bx bxl-twitter"></i>
+                  </a>
+                  <a href="/" class="facebook">
+                    <i class="bx bxl-facebook"></i>
+                  </a>
+                  <a href="/" class="instagram">
+                    <i class="bx bxl-instagram"></i>
+                  </a>
+                  <a href="/" class="google-plus">
+                    <i class="bx bxl-skype"></i>
+                  </a>
+                  <a href="/" class="linkedin">
+                    <i class="bx bxl-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-    </div>
-  </div>
-</div>
-{/* 
+        {/* 
 <div class="container py-4">
   <div class="copyright">
     &copy; Copyright <strong><span>Perfect Match</span></strong>. All Rights Reserved
   </div>
   
 </div> */}
-</footer>
-    
-    
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
