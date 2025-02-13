7379d7e34c6a33d6a81c213984e18e9aa261a698
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-13 22:39:49 +0530

    Update Hero.jsx

--- src/components/fcomponents/Hero.jsx
+++ src/components/fcomponents/Hero.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <>
-
-
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-      <div class="row gy-4">
-        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-          <h1>Find Your Perfect Match for a Lifetime of Happiness</h1>
-          <h2>Are you ready to take the next step towards finding your life partner?</h2>
-          <div>
-            <Link to="/signup" class="btn-get-started scrollto">Register today</Link>
+      <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+          <div class="row gy-4">
+            <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+              <h1>अपने जीवनभर की खुशियों के लिए अपना परफेक्ट साथी खोजें</h1>
+              <h2>
+                क्या आप अपने जीवनसाथी को पाने के अगले कदम के लिए तैयार हैं?
+              </h2>
+              <div>
+                <Link to="/signup" class="btn-get-started scrollto">
+                  आज ही पंजीकरण करें
+                </Link>
+              </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 hero-img">
+              <img src="" class="img-fluid animated" alt="" />
+            </div>
           </div>
         </div>
-        <div class="col-lg-6 order-1 order-lg-2 hero-img">
-          <img src="" class="img-fluid animated" alt=""/>
-        </div>
-      </div>
-    </div>
-
-  </section>
-    
-    
-    
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
