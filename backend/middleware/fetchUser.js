678f5e3997e28f8243cead8429ccbd57dcc64e97
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-14 10:13:33 +0530

    Update fetchUser.js

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,30 +1,21 @@
-
-var jwt = require('jsonwebtoken'); //JWT included for security
-
+var jwt = require("jsonwebtoken"); //JWT included for security
+const User = require("../models/User");
 const JWT_SECRET = "thisISveryImportant@forSecurity";
 
+const fetchuser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate with a valid token" });
+  }
 
-
-const fetchuser = (req, res, next)=>{
-
-
-    const token = req.header('auth-token');
-    if(!token)
-    {
-        res.status(401).send({error: "Please authenticate with a valid token"});
-    }
-
-    try {
-
-        const data = jwt.verify(token, JWT_SECRET);
-        req.user = data.user;
-        next();
-            
-    } 
-    
-    catch (error) {
-            res.status(401).send({error: "Please authenticate with a valid token"});
-    }
-}
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(data.user.id).select("-password");
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate with a valid token" });
+  }
+};
 
 module.exports = fetchuser;
